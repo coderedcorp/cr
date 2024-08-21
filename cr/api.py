8254310299945665b246466e92e121f5481c6a7d
Jeremy Childers <jchilders@coderedcorp.com>
2024-08-21 16:55:31 -0400

    Update param name for api change

--- cr/api.py
+++ cr/api.py
@@ -3,6 +3,7 @@ Utilities to call CodeRed Cloud API.
 
 Copyright (c) 2022 CodeRed LLC.
 """
+
 import json
 import time
 from http.client import HTTPResponse
@@ -319,7 +320,7 @@ class Webapp:
                 "webapp": self.id,
                 "env": self.env.value,
                 "task_type": "getlog",
-                "query_params": {"since": since},
+                "agent_args": {"since": since},
             },
         )
         if status >= 400:
@@ -345,7 +346,7 @@ class Webapp:
                 if line["source"] == "stderr":
                     style = "logging.level.warning"
                 progress.print(
-                    f"> {text}",
+                    f">{text}",
                     markup=False,
                     highlight=False,
                     style=style,
